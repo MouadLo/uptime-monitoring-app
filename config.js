b11476edc65d74c268bc93c181cf9ca7f80f573e
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-01-23 03:24:00 -0500

    Building the  Web App GUI

--- config.js
+++ config.js
@@ -1,32 +1,56 @@
 /*
  * Create and export configuration variables
- * 
-*/
+ *
+ */
 
-// Container for all the enviroments
-var enviroments = {};
+// Container for all environments
+var environments = {};
 
 // Staging (default) environment
-enviroments.staging =  {
-    'httpPort' : 3000,
-    'httpsPort' : 3001,
-    'envName': 'staging',
-    'hashingSecret': 'thisIsSecret'
+environments.staging = {
+  'httpPort' : 3000,
+  'httpsPort' : 3001,
+  'envName' : 'staging',
+  'hashingSecret' : 'thisIsASecret',
+  'maxChecks' : 5,
+  'twilio' : {
+    'accountSid' : 'ACb32d411ad7fe886aac54c665d25e5c5d',
+    'authToken' : '9455e3eb3109edc12e3d8c92768f7a67',
+    'fromPhone' : '+15005550006'
+  },
+  'templateGlobals' : {
+    'appName' : 'UptimeChecker',
+    'companyName' : 'NotARealCompany, Inc.',
+    'yearCreated' : '2020',
+    'baseUrl' : 'http://localhost:3000/'
+  }
 };
 
-// Production environment 
-enviroments.production = {
-    'httpPort' : 5000,
-    'httpsPort' : 5001,
-    'envName': 'production',
-    'hashingSecret': 'thisIsAlseSecret'
+// Production environment
+environments.production = {
+  'httpPort' : 5000,
+  'httpsPort' : 5001,
+  'envName' : 'production',
+  'hashingSecret' : 'thisIsAlsoASecret',
+  'maxChecks' : 10,
+  'twilio' : {
+    'accountSid' : '',
+    'authToken' : '',
+    'fromPhone' : ''
+  },
+  'templateGlobals' : {
+    'appName' : 'UptimeChecker',
+    'companyName' : 'NotARealCompany, Inc.',
+    'yearCreated' : '2020',
+    'baseUrl' : 'http://localhost:5000/'
+  }
 };
 
 // Determine which environment was passed as a command-line argument
-var cuurentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+var currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
 
-// Check that the current environment is one of the environment above if not  default to staging
-var enviromentsToExport = typeof(enviroments[cuurentEnvironment]) == 'object' ? enviroments[cuurentEnvironment] : enviroments.staging;
+// Check that the current environment is one of the environments above, if not default to staging
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
 
-// export the module
-module.exports = enviromentsToExport;
+// Export the module
+module.exports = environmentToExport;
